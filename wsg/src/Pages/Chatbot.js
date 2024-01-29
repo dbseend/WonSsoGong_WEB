808a685a367ffe306117ae16948914d31fc36db1
seol-wonjun <qiops@naver.com>
2024-01-29 23:26:50 +0900

    create Chatbot

---
+++ wsg/src/Pages/Chatbot.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import WebChatbot from "../Components/WebChatbot";
+import WebNavbar from "../Components/WebNavbar";
+
+const Chatbot = () => {
+  return (
+    <div>
+        {/* <WebNavbar /> */}
+        <WebChatbot />
+    </div>
+  );
+};
+
+export default Chatbot;

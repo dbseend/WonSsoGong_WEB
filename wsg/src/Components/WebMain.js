927f6f4b030faa0981317fe8b99b68dc552d9094
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-01-29 21:51:46 +0900

    setting

---
+++ wsg/src/Components/WebMain.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { useRecoilState } from "recoil";
+import { useNavigate } from "react-router-dom";
+
+const Main = () => {
+  return (<>Hello, World!</>);
+};
+
+export default Main;

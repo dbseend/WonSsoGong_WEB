f9fe6f9920925aa686ffc9d0add72f78c43173e6
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-01-31 00:18:53 +0900

    온보딩 디자인 중

--- wsg/src/App.js
+++ wsg/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Chatbot from "./Pages/Chatbot";
+import OnBoarding from "./Pages/OnBoarding";
 import Main from "./Pages/Main";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<OnBoarding />} />
+        <Route path="/main" element={<Main />} />
         <Route path="/chatbot" element={<Chatbot />} />
       </Routes>
     </Router>
---
+++ wsg/src/Components/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin : 0;
+    padding: 0;
+    width: 1512px;
+    height: 1964px;
+    background: #5379c2;
+    /* z-index: auto; */
+    display: flex;
+    flex-direction: column;
+    /* justify-content: center; */
+    align-items: center;
+  }
+`;
+export default GlobalStyle;

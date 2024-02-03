c4df6a951a2e8726dba8770876f1064c91cffcec
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-02-03 13:12:58 +0900

    onBoarding

--- wsg/src/App.js
+++ wsg/src/App.js
@@ -5,6 +5,8 @@ import Chatbot from "./Pages/Chatbot";
 import OnBoarding from "./Pages/OnBoarding";
 import Main from "./Pages/Main";
 import Chapter12 from "./Pages/Chapter12";
+import Chapter22 from "./Pages/Chapter2";
+import Chapter2 from "./Pages/Chapter2";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/main" element={<Main />} />
         <Route path="/chapter12" element={<Chapter12 />} />
         <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/chapter2" element={<Chapter2 />} />
+        <Route path="/chapter22" element={<Chapter22 />} />
       </Routes>
     </Router>
   );
--- wsg/src/Components/WebOnBoarding.js
+++ wsg/src/Components/WebOnBoarding.js
@@ -2,10 +2,6 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import GlobalStyle from "./GlobalStyle";
 import FluentArrow from "../Assets/FluentArrow.svg";
-import intersectImage1 from "../Assets/Intersect-3.png";
-import intersectImage2 from "../Assets/Intersect-1.png";
-import intersectImage3 from "../Assets/Intersect.png";
-import intersectImage4 from "../Assets/Intersect-2.png";
 import WebFooter from "./WebFooter";
 import { useNavigate } from "react-router-dom";
 
@@ -25,25 +21,21 @@ const WebOnBoarding = () => {
   const rectData = [
     {
       title: "법안 발의",
-      content: "사용자가 가상 국회의원이 되어 법안을 발의하는 시뮬레이션",
-      image: intersectImage1,
+      content: "가상의 국회의원이 되어보는 시간! AI 도우미 빵긋이와 쉽고 재미있게 법안을 발의해봐요."
     },
     {
       title: "토론 참여",
-      content: "다른 가상 의원들과 법안에 대해 토론 및 사용자 의견 제시",
-      image: intersectImage2,
+      content: "AI 가상 의원들과 발의한 법안에 대해 자유롭게 토론을 나누어보아요.",
     },
     {
       title: "투표 진행",
       content:
-        "토론 시간이 끝난 후 법안에 대한 최종 투표(찬성, 반대, 기권) 참여",
-      image: intersectImage3,
+        "토론과 투표 결과는 실제로 가상 국회에 영향을 미친답니다.",
     },
     {
       title: "법안 분석",
       content:
-        "내가 선택한 법안 주제와 같은 주제의 법안을 이해하기 쉬운 언어로 요약 및 정보 제공",
-      image: intersectImage4,
+        "현재 우리 국회에 어떤 법안들이 존재하는지 AI 도우미 빵긋이와 알아봐요",
     },
     // Add more data for other Rects
   ];
@@ -54,14 +46,16 @@ const WebOnBoarding = () => {
       const scrolledHeight = window.scrollY;
       const windowHeight = window.innerHeight;
       const documentHeight = document.documentElement.scrollHeight;
-      if (scrolledHeight + windowHeight >= documentHeight) { // 스크롤이 일정 높이 이상 내려갔을 때 Footer 표시
+      if (scrolledHeight + windowHeight >= documentHeight) {
+        // 스크롤이 일정 높이 이상 내려갔을 때 Footer 표시
         setShowFooter(true);
       } else {
         setShowFooter(false);
       }
     };
     window.addEventListener("scroll", handleScroll); // 스크롤 이벤트 리스너 등록
-    return () => { // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      // 컴포넌트 언마운트 시 이벤트 리스너 제거
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
@@ -70,16 +64,17 @@ const WebOnBoarding = () => {
     <Div>
       <GlobalStyle />
       <Top>
-      <StyledCircleWrapper>
-        <StyledSecondCircle/> {/* 그 위에 원 */}
-        <StyledCircle /> {/* 맨 아래 원 */}
-      </StyledCircleWrapper>
+        {/* <StyledCircleWrapper> */}
+        {/* <StyledSecondCircle/> 그 위에 원 */}
+        {/* <StyledCircle /> 맨 아래 원 */}
+        {/* </StyledCircleWrapper> */}
         <div>
-            <WhiteText>국회의 일부가 되어 국회 의원이 되어보는 경험,</WhiteText>{" "}
-            <GradientText> 방방국국!</GradientText>
-            <SubT>
-              가상 국회 의원으로서 법안을 발의하고, 토론에 참여하며, 투표하는 등의 다양한 활동을 체험할 수 있습니다.
-            </SubT>
+          <WhiteText>국회의 일부가 되어 국회 의원이 되어보는 경험,</WhiteText>{" "}
+          <GradientText> 방방국국!</GradientText>
+          <SubT>
+            가상 국회 의원으로서 법안을 발의하고, 토론에 참여하며, 투표하는 등의
+          </SubT>
+          <SubT>다양한 활동을 체험할 수 있습니다.</SubT>
         </div>
         <Button onClick={moveToMain}>시작하기</Button>
       </Top>
@@ -89,7 +84,6 @@ const WebOnBoarding = () => {
             key={index}
             title={data.title}
             content={data.content}
-            image={data.image}
           />
         ))}
       </RectArea>
@@ -98,15 +92,40 @@ const WebOnBoarding = () => {
     </Div>
   );
 };
+function splitBoldText(content, boldText) {
+  const parts = [];
+  let currentIndex = 0;
 
-const Rect = ({ title, content, image }) => (
-  <StyledRect>
-    <BillText className="bill-text">{title}</BillText>
-    <NewText className="new-text">{title}</NewText>
-    <NewSubText className="newSub-text">{content}</NewSubText>
-    <RectImage className=" rect-image" src={image} alt={title} />
-  </StyledRect>
-);
+  while (true) {
+    const boldStartIndex = content.indexOf(boldText, currentIndex);
+    if (boldStartIndex === -1) {
+      parts.push(content.slice(currentIndex));
+      break;
+    }
+
+    const beforeBoldText = content.slice(currentIndex, boldStartIndex);
+    parts.push(beforeBoldText);
+
+    parts.push(boldText);
+    currentIndex = boldStartIndex + boldText.length;
+  }
+
+  return parts;
+}
+
+
+const Rect = ({ title, content }) => {
+  const [boldText, normalText] = splitBoldText(content, "쉽고 재미있게");
+
+  return (
+    <StyledRect>
+      <BillText className="bill-text">{title}</BillText>
+      <NewText className="new-text">{title}</NewText>
+      <NewSubText className="newSub-text1">{normalText}</NewSubText>
+      <NewSubTextBold className="newSub-text-bold">{boldText}</NewSubTextBold>
+    </StyledRect>
+  );
+};
 
 const Div = styled.div`
   display: flex;
@@ -142,8 +161,7 @@ const SubT = styled.div`
   font-style: normal;
   font-weight: 400;
   line-height: normal;
-  margin-top: 32px;
-  white-space: pre-line; /* 줄 바꿈을 유지하도록 추가 */
+  /* margin-top: 32px; */
 `;
 
 const Button = styled.div`
@@ -156,20 +174,21 @@ const Button = styled.div`
   border-radius: 84px;
   background: linear-gradient(
     90deg,
-    #FFC634 0%,
-    #FFD34A 10%,
-    #FFEA5F 30%,
-    #FFF275 50%,
-    #D8F5DD 100%
+    #ffc634 0%,
+    #ffd34a 10%,
+    #ffea5f 30%,
+    #fff275 50%,
+    #d8f5dd 100%
   );
-  color: #5379C2;
+  color: #5379c2;
   font-family: "Pretendard Variable";
   font-size: 25.2px;
   font-style: normal;
   font-weight: 800;
   line-height: 40px;
   text-align: center;
-  margin-top: 65px;
+  margin-top: 50px;
+  margin-bottom: 10px;
   cursor: pointer;
 `;
 
@@ -205,14 +224,14 @@ const StyledRect = styled.div`
     backdrop-filter 0.3s ease;
 
   &::before {
-    content: url(${(props) => props.image});
+    /* content: url(${(props) => props.image});
     display: block;
     position: absolute;
     top: 25%;
     left: 50%;
     transform: translate(-50%, -50%);
     opacity: 0;
-    transition: opacity 0.5s ease;
+    transition: opacity 0.5s ease; */
   }
 
   &:hover {
@@ -228,18 +247,17 @@ const StyledRect = styled.div`
     &::before {
       opacity: 1;
     }
-    .rect-image {
-      opacity: 1;
-    }
     .bill-text {
       opacity: 0;
     }
-
     .new-text {
       opacity: 1;
     }
-
     .newSub-text {
+      opacity: 1;
+    }
+
+    .newSub-text-bold {
       opacity: 1;
     }
   }
@@ -252,7 +270,7 @@ const StyledCircle = styled.div`
   flex-shrink: 0;
   border-radius: 50%;
   opacity: 0.4;
-  background: linear-gradient(180deg, rgba(246, 246, 246, 0.00) 54%, #F6F6F6 100%);
+  background: linear-gradient(180deg, rgba(246, 246, 246, 0) 54%, #f6f6f6 100%);
 `;
 
 const StyledCircleWrapper = styled.div`
@@ -268,23 +286,15 @@ const StyledSecondCircle = styled.div`
   top: calc(50% - 50px); /* 중앙에서 100px 위로 이동 */
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 2290px;
-  height: 1000px;
+  width: 100%;
+  height: 100%;
+  /* width: 2290px;
+  height: 1000px; */
   flex-shrink: 0;
   border-radius: 50%;
   opacity: 0.4;
-  background: linear-gradient(180deg, rgba(246, 246, 246, 0.00) 54%, #F6F6F6 100%);
+  background: linear-gradient(180deg, rgba(246, 246, 246, 0) 54%, #f6f6f6 100%);
   z-index: -2; /* 다른 요소보다 뒤에 위치하도록 설정 */
-`;
-
-const RectImage = styled.img`
-  opacity: 0;
-  position: absolute;
-  top: 20%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  transition: opacity 0.5s ease;
-  z-index: 0;
 `;
 
 const BillText = styled.div`
@@ -296,10 +306,10 @@ const NewText = styled.div`
   opacity: 0;
   transition: opacity 0.5s ease;
   position: absolute;
-  top: 12.5%;
+  top: 12%;
   left: 50%;
   transform: translate(-50%, -50%);
-  color: var(--Primary, #ffa130);
+  color: var(--Primary, #F6F6F6);
   text-shadow: 1px 1px 1px #000;
   font-family: "Pretendard Variable";
   font-size: 24px;
@@ -324,7 +334,21 @@ const NewSubText = styled.div`
   font-weight: 400;
   line-height: normal;
 `;
-
+const NewSubTextBold = styled.div`
+  opacity: 0;
+  transition: opacity 0.5s ease;
+  position: absolute;
+  top: 45.5%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 152px;
+  color: #000;
+  font-family: "Pretendard Variable";
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+`;
 const Arrow = styled.img`
   margin-top: 750px;
   align-self: center;
@@ -338,15 +362,16 @@ const GradientText = styled.div`
   display: inline-block;
   background: linear-gradient(
     90deg,
-    #FFC634 0%,
-    #FFD34A 10%,
-    #FFEA5F 30%,
-    #FFF275 50%,
-    #D8F5DD 100%
+    #ffc634 0%,
+    #ffd34a 10%,
+    #ffea5f 30%,
+    #fff275 50%,
+    #d8f5dd 100%
   );
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+  margin-bottom: 40px;
 `;
 
 const WhiteText = styled.span`

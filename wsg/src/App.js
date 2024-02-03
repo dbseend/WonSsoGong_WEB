18ce1f9afbc903a8257c2128196ee5ae4571e63e
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-02-03 13:52:56 +0900

    Kye (#11)
    * fixing
    
    * 온보딩 디자인 중
    
    * Intersect, FluentArrow  Design
    
    * Chapter1
    
    * 커밋하래
    
    * onBoarding
    
    * 이미지
    
    * 이미지 테스트 온보딩페이지)

--- wsg/src/App.js
+++ wsg/src/App.js
@@ -5,8 +5,8 @@ import Chatbot from "./Pages/Chatbot";
 import OnBoarding from "./Pages/OnBoarding";
 import Main from "./Pages/Main";
 import Chapter12 from "./Pages/Chapter12";
+import Chapter22 from "./Pages/Chapter2";
 import Chapter2 from "./Pages/Chapter2";
-import Chapter22 from "./Pages/Chapter22";
 
 function App() {
   return (
@@ -15,9 +15,9 @@ function App() {
         <Route path="/" element={<OnBoarding />} />
         <Route path="/main" element={<Main />} />
         <Route path="/chapter12" element={<Chapter12 />} />
+        <Route path="/chatbot" element={<Chatbot />} />
         <Route path="/chapter2" element={<Chapter2 />} />
         <Route path="/chapter22" element={<Chapter22 />} />
-        <Route path="/chatbot" element={<Chatbot />} />
       </Routes>
     </Router>
   );

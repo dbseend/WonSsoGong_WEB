31ed1e9eab366ef2f32412ea527c62deff353f80
seol-wonjun <qiops@naver.com>
2024-02-06 01:11:33 +0900

    update Footer and Navbar

--- wsg/src/Components/Etc/WebFooter.js
+++ wsg/src/Components/Etc/WebFooter.js
@@ -18,7 +18,7 @@ const FooterContainer = styled.div`
     flex-shrink: 0;
     background: var(--secondary-bg, #77A1F5);
     backdrop-filter: blur(10px);
-    width: 1450px;
+    width: 100%;
     bottom: 0;
     left: 0;
 `;

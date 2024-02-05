53bdb5302a37038c927ff9a6b4fa86536e263622
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-02-06 02:06:48 +0900

    Kye (#29)
    * fixing
    
    * 온보딩 디자인 중
    
    * Intersect, FluentArrow  Design
    
    * Chapter1
    
    * 커밋하래
    
    * onBoarding
    
    * 이미지
    
    * 이미지 테스트 온보딩페이지)
    
    * 이미지 배치, 빵긋이 호버
    
    * 온보딩 완성
    
    * 별거안함
    
    * api
    
    * 온보딩, 법안 발의 페이지 디자인 수정 및 API 연결
    
    * check
    
    * 재클론 확인
    
    * api 테스트
    
    * 음..
    
    * 온보딩^^
    
    * 배치 공사 중
    
    * Revert "Ysh (#25)"
    
    This reverts commit f5b5c3dd4d51439e06eb106cfa701f0437cea863.
    
    * 커밋하란다
    
    * 메인페이지 정렬

---
+++ wsg/src/Pages/Tutorial2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import WebFooter from "../Components/WebFooter";
+import WebNavbar from "../Components/WebNavbar";
+import WebTutorial2 from "../Components/WebTutorial2";
+
+const Tutorial = () => {
+  return (
+    <div>
+      <WebNavbar />
+      <WebTutorial2 />
+      <WebFooter />
+    </div>
+  );
+};
+
+export default Tutorial;

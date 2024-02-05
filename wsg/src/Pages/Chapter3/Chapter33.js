e23632a9ea9ebacaddae8f3b5487a93153224c68
dbseend <shyoon840@gmail.com>
2024-02-06 00:44:28 +0900

    폴더 구조 정리

---
+++ wsg/src/Pages/Chapter3/Chapter33.js
@@ -0,0 +1,16 @@
+import React from "react";
+import WebFooter from "../../Components/Etc/WebFooter";
+import WebNavbar from "../../Components/Etc/WebNavbar";
+import WebChapter33 from "../../Components/Chapter3/WebChapter33";
+
+const Chapter33 = () => {
+  return (
+    <div>
+      <WebNavbar />
+      <WebChapter33 />
+      <WebFooter />
+    </div>
+  );
+};
+
+export default Chapter33;

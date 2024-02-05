97dc2cb67394baa79abc561b7c3fd97aaea23cd3
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-02-06 02:20:43 +0900

    Revert "Kye (#29)"
    This reverts commit 53bdb5302a37038c927ff9a6b4fa86536e263622.

---
+++ wsg/src/Pages/Chapter2/Chapter22.js
@@ -0,0 +1,16 @@
+import React from "react";
+import WebFooter from "../../Components/Etc/WebFooter";
+import WebNavbar from "../../Components/Etc/WebNavbar";
+import WebChapter22 from "../../Components/Chapter2/WebChapter22";
+
+const Chapter22 = () => {
+  return (
+    <div>
+      <WebNavbar />
+      <WebChapter22 />
+      <WebFooter />
+    </div>
+  );
+};
+
+export default Chapter22;
---
+++ wsg/src/Pages/Tutorial/Tutorial3.js
@@ -0,0 +1,16 @@
+import React from "react";
+import WebFooter from "../../Components/Etc/WebFooter";
+import WebNavbar from "../../Components/Etc/WebNavbar";
+import WebTutorial3 from "../../Components/Tutorial/WebTutorial3";
+
+const Tutorial3 = () => {
+  return (
+    <div>
+      <WebNavbar />
+      <WebTutorial3 />
+      <WebFooter />
+    </div>
+  );
+};
+
+export default Tutorial3;

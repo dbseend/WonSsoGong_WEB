f5b5c3dd4d51439e06eb106cfa701f0437cea863
dbseend <141024382+dbseend@users.noreply.github.com>
2024-02-06 00:45:02 +0900

    Ysh (#25)
    * 배포
    
    * 폴더 구조 정리

---
+++ wsg/src/Pages/Chapter3/Chapter3Result1.js
@@ -0,0 +1,16 @@
+import React from "react";
+import WebFooter from "../../Components/Etc/WebFooter";
+import WebNavbar from "../../Components/Etc/WebNavbar";
+import WebChapter3Result1 from "../../Components/Chapter3/WebChapter3Result1";
+
+const Chapter3Result1 = () => {
+  return (
+    <div>
+      <WebNavbar />
+      <WebChapter3Result1 />
+      <WebFooter />
+    </div>
+  );
+};
+
+export default Chapter3Result1;

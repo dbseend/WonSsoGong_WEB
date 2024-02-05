798e9bcbaa0dc42a52a01757760eb35d963a826f
KKIMYENN <yenny031003@gmail.com>
2024-02-06 02:17:22 +0900

    ㅁㅇㅍㅇㅈ ㅈㄹ

--- wsg/src/Components/Chapter2/WebChapter2.js
+++ wsg/src/Components/Chapter2/WebChapter2.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import GlobalStyle from "./GlobalStyle";
+import { useNavigate } from "react-router-dom";
 import Group117 from "../../Assets/Group 117.svg";
 import Group68 from "../../Assets/Group 68.png";
 import { Button } from "../Chapter1/WebChapter12";
 import GlobalStyle from "../Etc/GlobalStyle";
+import BG from "../../Assets/BG.svg";
 import {
-  BalloonContainer,
-  ChapTitle,
-  ChapterArea,
   Div,
-  Title
+  ChapterArea,
+  ChapTitle,
+  Title,
+  BubbleText,
 } from "../Main/WebMain";
 import { Background } from "../Main/WebOnBoarding";
+import styled from "styled-components";
 
 const WebChapter2 = () => {
   const navigate = useNavigate();
@@ -23,53 +26,25 @@ const WebChapter2 = () => {
   return (
     <Div>
       <GlobalStyle />
-      <Background />
-
+      <Background src={BG} />
       <ChapterArea>
         <ChapTitle>Chapter 2.</ChapTitle>
         <Title>토론 참여</Title>
       </ChapterArea>
 
       <BalloonContainer>
-        <Group117Image src={Group117} alt="Chapter 2 Group117" />
-        {/*         <BubbleText src={Group118} alt="Main Group118" />
-        <ChapterChar src={Group63} alt="Main Group63" /> */}
+        <BubbleText src={Group117} alt="Chapter 2 Group117" />
+        <Chapter2Char src={Group68} alt="Chapter 2 Group68" />
       </BalloonContainer>
-      <Group68Image src={Group68} alt="Chapter 2 Group68" />
+
       <Button onClick={moveToNext}>다음으로 넘어가기</Button>
     </Div>
   );
-  /*  return (
-    <>
-      <GlobalStyle />
-      <Background />
-      <ChapTitle>Chapter 2.</ChapTitle>
-      <Title>토론 참여</Title>
-      <BalloonContainer>
-        <BalloonContent>
-          <Group117Image src={Group117} alt="Chapter 2 Group117" />
-        </BalloonContent>
-      </BalloonContainer>
-      <SubText></SubText>
-      <Group68Image src={Group68} alt="Chapter 2 Group68" />
-      <Button onClick={moveToNext}>다음으로 넘어가기</Button>
-    </>
-  ); */
 };
 
-const Group68Image = styled.img`
-  position: absolute;
-  top: 210px;
-  right: 300px;
-  width: 300px;
-  height: 400px;
+const Chapter2Char = styled.img`
+  margin-top: 50px;
+  width: 359px;
+  height: 466.45px;
 `;
-
-const Group117Image = styled.img`
-  width: 550px;
-  height: 500px; /* 이미지가 70%의 높이를 차지하도록 설정 */
-`;
-
-export { Button };
-
 export default WebChapter2;

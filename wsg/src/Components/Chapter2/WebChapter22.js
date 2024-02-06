df815087189053245564b13c212d52fffd06ebc4
KKIMYENN <yenny031003@gmail.com>
2024-02-07 03:39:47 +0900

    채팅 구현 중

--- wsg/src/Components/Chapter2/WebChapter22.js
+++ wsg/src/Components/Chapter2/WebChapter22.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import {
   SmallBackground,
@@ -8,29 +8,49 @@ import {
   ChapTitle,
   Part0,
   Title,
-  Type,
-  TypeArea,
-  SubTitle,
-  Container,
-  Part1,
 } from "../Chapter1/WebChapter12";
 import smallBg from "../../Assets/bg1.svg";
 import GlobalStyle from "../Etc/GlobalStyle";
-import AiChar from "../../Assets/AiChar.png";
-import HumanChar from "../../Assets/HumanChar.png";
-
+import AiChar from "../../Assets/Group 167.png";
+import HumanChar from "../../Assets/Group 168.png";
+import { debate } from "../../Api/BillAPI";
 
 const WebChapter22 = () => {
   const [messages, setMessages] = useState([]);
-  const [aiMessages, setAiMessages] = useState([
-    "나는",
-    "기계",
-    "인간",
-    "멍청하다",
-  ]);
+  const [aiMessages, setAiMessages] = useState([]);
   const [userInput, setUserInput] = useState("");
-  // const [content, setContent] = useState("");
+  const [aiAnswer, setAiAnswer] = useState("");
+  const [converSation, setConversation] = useState([]);
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
 
+  const handleSendMessage = async () => {
+    if (userInput.trim() !== "") {
+      const userMessage = { text: userInput, sender: "user" };
+      setMessages([...messages, userMessage]);
+
+      try {
+        const answer = await debate(userInput);
+        const aiMessage = { text: answer, sender: "Ai" };
+
+        setAiAnswer(answer);
+        setAiMessages([...aiMessages, aiMessage]);
+        setConversation([...converSation, userMessage, aiMessage]);
+      } catch (error) {
+        console.error("Error while fetching data:", error);
+      }
+      setUserInput("");
+    }
+  };
+
+  useEffect(() => {
+    console.log(messages);
+  }, [messages]);
+
+  useEffect(() => {
+    console.log(aiMessages);
+  }, [aiMessages]);
   return (
     <>
       <Div>
@@ -42,20 +62,43 @@ const WebChapter22 = () => {
           <Title>토론 참여</Title>
         </Part0>
         <Hr />
-        <CenteredContainer>
-          {messages.map((message, index) => (
-            <SpeechBubble key={index}>{message}</SpeechBubble>
-          ))}{" "}
-        </CenteredContainer>
 
+        <Container>
+          <AiCharImg src={AiChar} />
+          <HumanCharImg src={HumanChar} />
+        </Container>
+
+        <CenteredContainer>
+          <OppositeMessageBubble className="push">
+            가상 국회의원들과 토론을 진행하세요. 먼저 의견을 내면 토론이
+            시작됩니다.
+          </OppositeMessageBubble>
+          {messages.map((message, index) => (
+            <MessageBubble
+              className={message.sender === "user" ? "pull" : "push"}
+              key={index}
+            >
+              {message.text}
+            </MessageBubble>
+          ))}
+
+          {aiMessages.map((aiMessage, index) => (
+            <OppositeMessageBubble
+              className={aiMessage.sender === "Ai" ? "pull" : "push"}
+              key={index}
+            >
+              {aiMessage.text}
+            </OppositeMessageBubble>
+          ))}
+        </CenteredContainer>
         <SendArea>
           <ContentInput
             type="text"
             placeholder="발언을 입력하세요."
             value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
+            onChange={handleInputChange}
           />
-          <SendButton>전송</SendButton>
+          <SendButton onClick={handleSendMessage}>전송</SendButton>
         </SendArea>
         <Button>토론 끝내기</Button>
       </Div>
@@ -63,56 +106,105 @@ const WebChapter22 = () => {
   );
 };
 
-const ContentInput = styled.input`
-  width: 800px;
-  margin-left: 88px;
-  padding: 10px;
-  font-size: 16px;
-  border: none;
-  background: rgba(246, 246, 246, 0.3);
-  box-shadow: 0px 10px 100px rgba(0, 0, 0, 0.1); /* 그림자 추가 */
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 600px;
+  margin-top: 130px;
+  margin-bottom: 20px;
+  z-index: 1;
 `;
 
 const CenteredContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  padding-right: 30px;
+  z-index: 1000;
+  margin-top: -35%;
+  width: 45%;
+  height: 300px;
+  margin-bottom: 90px;
+  overflow: scroll;
+  overflow-y: auto;
+  overflow-x: hidden;
 `;
 
-const SpeechBubble = styled.div`
-  margin-left: 450px;
-  margin-bottom: 50px;
-  position: relative;
-  background: #ffc634;
-  border-radius: 0.4em;
+const MessageBubble = styled.div`
   width: auto;
   height: auto;
-  padding: 20px;
-
-  &:after {
+  max-width: 300.976px;
+  flex-shrink: 0;
+  background-color: rgba(255, 198, 52, 0.7);
+  filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.1));
+  margin-bottom: 29.56px;
+  padding-top: 7.34px;
+  padding-bottom: 7.34px;
+  padding-left: 13.92px;
+  padding-right: 13.92px;
+  margin-right: -85%;
+  &.pull:after {
+    align-self: flex-end;
     content: "";
     position: absolute;
-    right: 5px;
-    top: 50%;
-    /* width: 0; */
-    /* height: 0; */
-    border: 16px solid transparent;
-    border-left-color: #ffc634;
-    border-right: 0;
-    border-top: 0;
-    margin-top: -15px;
-    margin-right: -16px;
+    top: 0px;
+    right: -40px;
+    border-bottom: 10px solid transparent;
+    border-right: 20px solid transparent;
+    border-left: 20px solid #ffc634;
+    border-top: 0px solid #ffc634;
+    opacity: 0.7;
   }
 `;
-
+const OppositeMessageBubble = styled.div`
+  width: auto;
+  height: auto;
+  max-width: 300.976px;
+  flex-shrink: 0;
+  background-color: rgba(246, 246, 246, 0.3);
+  filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.1));
+  margin-bottom: 29.56px;
+  padding-top: 7.34px;
+  padding-bottom: 7.34px;
+  padding-left: 13.92px;
+  padding-right: 13.92px;
+  margin-left: -25%;
+  &.push:after {
+    align-self: flex-start;
+    content: "";
+    position: absolute;
+    top: 0px;
+    left: -20px;
+    border-top: 0px solid #f6f6f64d;
+    border-right: 20px solid #f6f6f64d;
+    border-left: 20px solid #transparent;
+    border-bottom: 10px solid transparent;
+  }
+`;
 const SendArea = styled.div`
   display: flex;
   flex-direction: row;
+  margin-bottom: 30px;
+  gap: 30px;
+  z-index: 1000;
+`;
+const ContentInput = styled.input`
+  width: 800px;
+  padding-left: 20px;
+  font-size: 18px;
+  border: none;
+  background: rgba(246, 246, 246, 0.3);
+  box-shadow: 0px 10px 100px rgba(0, 0, 0, 0.1); /* 그림자 추가 */
+  &::placeholder {
+    color: #fff;
+  }
+  color: #fff;
 `;
 const SendButton = styled.button`
-  width: 113px;
-  padding: 10px 40px;
+  width: 149px;
+  height: 40px;
+  flex-shrink: 0;
   background-color: rgba(255, 198, 52, 0.8);
   color: white;
   border: none;
@@ -120,7 +212,17 @@ const SendButton = styled.button`
   cursor: pointer;
   flex-shrink: 0;
   font-weight: bold; /* 볼드체로 설정 */
+  font-size: 20px;
   box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.1); /* 그림자 추가 */
 `;
 
+const AiCharImg = styled.img`
+  width: 197.47px;
+  height: 290px;
+`;
+
+const HumanCharImg = styled.img`
+  width: 189px;
+  height: 284px;
+`;
 export default WebChapter22;
--- wsg/src/Components/Tutorial/WebTutorial4.js
+++ wsg/src/Components/Tutorial/WebTutorial4.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import Group156 from "../../Assets/Group 156.png";
-import Group167 from "../../Assets/Group 167.png";
+// import Group156 from "../../Assets/Group 156.png";
+// import Group167 from "../../Assets/Group 167.png";
 import GlobalStyle from "../Etc/GlobalStyle";
 import { SmallBackground } from "../Chapter1/WebChapter12";
 import smallBg from "../../Assets/bg1.svg";
@@ -18,19 +18,26 @@ const WebTutorial4 = () => {
     <Div>
       <GlobalStyle />
       <SmallBackground src={smallBg} />
-      <Group36Image src={Group156} alt="Tutorial Group36" />
-      <Group155Image src={Group167} alt="Tutorial Group135" />
+      {/* <Group36Image src={Group156} alt="Tutorial Group36" />
+      <Group155Image src={Group167} alt="Tutorial Group135" /> */}
       <Overlay>
         <NameText>빵긋이</NameText>
-        <WelcomeText>국회는 <BoldText>국민을 대표하여 나라를 운영하고 법을 만들어 나가는</BoldText> <br />
-          중요한 기관입니다. 국회는 <BoldText>국가의 정책 방향을 결정</BoldText>하고, <br />
-          <BoldText>국민들의 다양한 의견을 수렴</BoldText>하여 나라를 향상시키기 위해 노력하죠.</WelcomeText>
+        <WelcomeText>
+          국회는{" "}
+          <BoldText>
+            국민을 대표하여 나라를 운영하고 법을 만들어 나가는
+          </BoldText>{" "}
+          <br />
+          중요한 기관입니다. 국회는 <BoldText>국가의 정책 방향을 결정</BoldText>
+          하고, <br />
+          <BoldText>국민들의 다양한 의견을 수렴</BoldText>하여 나라를 향상시키기
+          위해 노력하죠.
+        </WelcomeText>
       </Overlay>
-        <Button onClick={moveToNext}>다음</Button>
+      <Button onClick={moveToNext}>다음</Button>
     </Div>
   );
 };
-
 
 const Overlay = styled.div`
   display: flex; /* 자식 요소들을 가로로 배치 */
@@ -39,8 +46,8 @@ const Overlay = styled.div`
   width: 900px;
   height: 167px;
   border-radius: 2px;
-  background: rgba(246, 246, 246, 0.30);
-  box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.10);
+  background: rgba(246, 246, 246, 0.3);
+  box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(25px);
   opacity: 0.9;
   margin-top: 4%; /* Adjust the top position according to your layout */

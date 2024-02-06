a0987af0aadc1914e363c42389925421bb27e257
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-02-06 21:35:56 +0900

    Kye (#44)
    * fixing
    
    * 온보딩 디자인 중
    
    * Intersect, FluentArrow  Design
    
    * Chapter1
    
    * 커밋하래
    
    * onBoarding
    
    * 이미지
    
    * 이미지 테스트 온보딩페이지)
    
    * 이미지 배치, 빵긋이 호버
    
    * 온보딩 완성
    
    * 별거안함
    
    * api
    
    * 온보딩, 법안 발의 페이지 디자인 수정 및 API 연결
    
    * check
    
    * 재클론 확인
    
    * api 테스트
    
    * 음..
    
    * 온보딩^^
    
    * 배치 공사 중
    
    * Revert "Ysh (#25)"
    
    This reverts commit f5b5c3dd4d51439e06eb106cfa701f0437cea863.
    
    * 커밋하란다
    
    * 메인페이지 정렬
    
    * ㅁㅇㅍㅇㅈ ㅈㄹ
    
    * 수습 끝
    
    * 에러 수정
    
    * Merge branch 'kye' of https://github.com/dbseend/WonSsoGong_WEB into kye
    
    * 법안발의 페이지, 페이지네이션
    
    * 디버깅
    
    * 온보딩 페이지 수정하다git add .!
    
    * 챕터 4

--- wsg/src/Components/Chapter4/WebChapter41.js
+++ wsg/src/Components/Chapter4/WebChapter41.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import GlobalStyle from "../Etc/GlobalStyle";
 import { useNavigate } from "react-router-dom";
+import Left from "../../Assets/Left.svg";
+import Right from "../../Assets/Right.svg";
 import {
   Hr,
   Div,
@@ -15,6 +17,8 @@ import BG from "../../Assets/BG.svg";
 import { Background } from "../Main/WebOnBoarding";
 import Chapter4Img from "../../Assets/Chapter4Char.png";
 import Chapter4Bubble from "../../Assets/Chapter4Bubble.svg";
+import searchIcon from "../../Assets/searchIcon.svg";
+
 const WebChapter41 = () => {
   const [keyword, setKeyword] = useState("");
   const types = ["재난", "안전", "도시", "교통", "주민", "법률"];
@@ -32,6 +36,18 @@ const WebChapter41 = () => {
 
   // 더미데이터
   const boardInfo = [
+    { id: 1238, author: "김철수", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1239, author: "김철수", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1240, author: "김예은", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1238, author: "김철수", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1239, author: "김철수", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1240, author: "김예은", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1238, author: "김철수", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1239, author: "김철수", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1240, author: "김예은", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1238, author: "김철수", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1239, author: "김철수", title: "00에 대한 법률 (XXX에 대하여)" },
+    { id: 1240, author: "김예은", title: "00에 대한 법률 (XXX에 대하여)" },
     { id: 1238, author: "김철수", title: "00에 대한 법률 (XXX에 대하여)" },
     { id: 1239, author: "김철수", title: "00에 대한 법률 (XXX에 대하여)" },
     { id: 1240, author: "김예은", title: "00에 대한 법률 (XXX에 대하여)" },
@@ -55,15 +71,20 @@ const WebChapter41 = () => {
 
   const Pagination = () => (
     <div>
-      <button Onclick={() => handlePageClick(1)}>첫 페이지</button>
+      <img src={Left} onClick={() => handlePageClick(1)} />
       {Array.from({ length: totalPages }).map((_, index) => (
-        <button key={index + 1} onClick={() => handlePageClick(index + 1)}>
+        <PaginationButton
+          key={index + 1}
+          selected={currentPage === index + 1} // Assuming currentPage is the current selected page
+          onClick={() => handlePageClick(index + 1)}
+        >
           {index + 1}
-        </button>
+        </PaginationButton>
       ))}
-      <button onClick={() => handlePageClick(totalPages)}>끝 페이지</button>
+      <img src={Right} onClick={() => handlePageClick(totalPages)} />
     </div>
   );
+  
   const handleKeywordChange = (type) => {
     setKeyword(type);
   };
@@ -90,10 +111,13 @@ const WebChapter41 = () => {
                 </Type>
               ))}
             </TypeArea>
-            <StyledSearch
-              type="search"
-              placeholder="법안 주제를 검색해주세요."
-            />
+            <SearchArea>
+              <StyledSearchIcon src={searchIcon} />
+              <StyledSearch
+                type="text"
+                placeholder="법안 주제를 검색해주세요."
+              />
+            </SearchArea>
           </Container>
         </Part1>
         <Part2>
@@ -101,32 +125,43 @@ const WebChapter41 = () => {
             <StyledChapter4Bubble src={Chapter4Bubble} />
             <StyledChapter4Char src={Chapter4Img} />
           </ImgGroup>
-
-          <StyledTable>
-            <thead>
-              <tr>
-                {columns.map((column) => (
-                  <th key={column.key}> {column.label} </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {visibleItems.map((row, rowIndex) => (
-                <tr key={rowIndex}>
+          <TableArea>
+            <StyledTable>
+              <thead>
+                <tr>
                   {columns.map((column) => (
-                    <td key={column.key}>{row[column.key]}</td>
+                    <th key={column.key}> {column.label} </th>
                   ))}
                 </tr>
-              ))}
-            </tbody>
-          </StyledTable>
+              </thead>
+              <tbody>
+                {visibleItems.map((row, rowIndex) => (
+                  <tr key={rowIndex}>
+                    {columns.map((column) => (
+                      <td key={column.key}>{row[column.key]}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </StyledTable>
 
-          <Pagination />
+            <Pagination />
+          </TableArea>
         </Part2>
       </Div>
     </>
   );
 };
+
+const PaginationButton = styled.button`
+background-color: transparent;
+  margin: 0 4px;
+  border: none;
+  cursor: pointer;
+  font-size: 14px;
+  color: ${(props) => (props.selected ? "#FFC634" : "#fff")};
+`;
+
 const Title = styled.div`
   color: #fff;
   font-family: "Pretendard Variable";
@@ -137,29 +172,49 @@ const Title = styled.div`
   z-index: 2;
   margin-left: -75%;
 `;
+const SearchArea = styled.div`
+  display: flex;
+  margin-right:1.5%;
+  position: relative;
+`;
+
+const StyledSearchIcon = styled.img`
+  position: absolute; 
+  top: 65%;
+  left: 20px;
+  transform: translateY(-50%);
+  z-index: 2;
+`;
 const StyledSearch = styled.input`
   margin-top: 20px;
-  width: 877px;
+  width: 810px;
   height: 44px;
   background-color: #f6f6f6;
-  opacity: 0.3;
   border: transparent;
-  z-index: 2;
+  z-index: 1;
+  color: #f6f6f6;
+  font-size: 18px;
+  line-height: 44px;
+
   &:: placeholder {
     color: #f6f6f6;
   }
+  background-color: #b0c8f6;
+  padding-left: 70px;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
 `;
 
 const Part2 = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
+  margin-right:15.5%;
+  gap: 40px;
+  margin-top: 2%;
 `;
 const ImgGroup = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 30px;
-/
 `;
 const StyledChapter4Bubble = styled.img`
   width: 285px;
@@ -172,6 +227,60 @@ const StyledChapter4Char = styled.img`
 `;
 
 const StyledTable = styled.table`
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+  width: 880px;
   z-index: 2;
+  text-align: left;
+  border-collapse: collapse;
+  font-family: "Pretendard Variable";
+
+  th,
+  td {
+    width: 874px;
+    padding: 10px;
+    padding-left: 30px;
+  }
+
+  th {
+    position: sticky;
+    top: 0;
+    font-weight: normal;
+    background-color: #c9d8f6;
+    text-align: left;
+    color: #5379c2;
+    z-index: 2;
+  }
+
+  td:first-child,
+  td:nth-child(2) {
+    width: 10%;
+  }
+
+  td:nth-child(3) {
+    width: 50%;
+  }
+
+  tr {
+    height: 50px;
+    background-color: #acc3f1;
+  }
+
+  tr:not(:last-child) {
+    box-shadow: inset 0 -2px #c9d8f6;
+  }
+
+  tr:hover {
+    background-color: #c9d8f6;
+    cursor: pointer;
+  }
+`;
+
+
+const TableArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap:15px;
 `;
 export default WebChapter41;

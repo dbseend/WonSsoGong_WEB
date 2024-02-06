57a28015dde036d1d58ed2ff7404fab258afb6f4
KKIMYENN <yenny031003@gmail.com>
2024-02-07 06:43:27 +0900

    채팅 구현 끝git add .git add .git add .!

--- wsg/src/Components/Chapter2/WebChapter22.js
+++ wsg/src/Components/Chapter2/WebChapter22.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import {
   SmallBackground,
@@ -14,6 +14,10 @@ import GlobalStyle from "../Etc/GlobalStyle";
 import AiChar from "../../Assets/Group 167.png";
 import HumanChar from "../../Assets/Group 168.png";
 import { debate } from "../../Api/BillAPI";
+import { useRecoilState } from "recoil";
+import { debateContents } from "../../Recoil/atom";
+import TimerVector from "../../Assets/Timer.svg";
+import { useNavigate } from "react-router-dom";
 
 const WebChapter22 = () => {
   const [messages, setMessages] = useState([]);
@@ -21,30 +25,51 @@ const WebChapter22 = () => {
   const [userInput, setUserInput] = useState("");
   const [aiAnswer, setAiAnswer] = useState("");
   const [conversation, setConversation] = useState([]);
+  const containerRef = useRef(null);
+  const [debateContentsState, setDebateContentsState] =
+    useRecoilState(debateContents);
+  const [isDebateStarted, setIsDebateStarted] = useState(false);
+  const [count, setCount] = useState(5);
+  const [minutes, setMinutes] = useState(Math.floor(count / 60));
+  const [seconds, setSeconds] = useState(count % 60);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isDebateStarted) {
+      const id = setInterval(() => {
+        setCount((prevCount) => prevCount - 1);
+      }, 1000);
+      // 토론 시간이 종료되면 clearInterval을 호출하여 타이머를 멈춥니다.
+      setTimeout(() => {
+        clearInterval(id);
+        setIsDebateStarted(false); // 토론 종료 시 상태 업데이트 등 추가 로직을 수행할 수 있습니다.
+        moveToNext();
+      }, count * 1000);
+      return () => clearInterval(id);
+    }
+  }, [count]);
+
+  // 분과 초 업데이트
+  useEffect(() => {
+    setMinutes(Math.floor(count / 60));
+    setSeconds(count % 60);
+  }, [count]);
+
+  // mm:ss 형태의 문자열 생성
+  const formattedTime = `${String(minutes).padStart(2, "0")}:${String(
+    seconds
+  ).padStart(2, "0")}`;
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
 
-  const handleSendMessage = async () => {
-    if (userInput.trim() !== "") {
-      const userMessage = { text: userInput, sender: "user" };
-      setMessages([...messages, userMessage]);
-  
-      try {
-        const answer = await debate(userInput);
-        const aiMessage = { text: answer, sender: "Ai" };
-  
-        setAiAnswer(answer);
-        setAiMessages([...aiMessages, aiMessage]);
-        setConversation([...conversation, userMessage, aiMessage]);
-      } catch (error) {
-        console.error("Error while fetching data:", error);
-      }
-  
-      setUserInput("");
-    }
-  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [conversation]);
 
+  const scrollToBottom = () => {
+    containerRef.current.scrollTop = containerRef.current.scrollHeight;
+  };
   useEffect(() => {
     console.log(messages);
   }, [messages]);
@@ -52,42 +77,92 @@ const WebChapter22 = () => {
   useEffect(() => {
     console.log(aiMessages);
   }, [aiMessages]);
+
+  const moveToNext = () => {
+    navigate("/chapter3/1");
+  }
+ 
+  const handleSendMessage = async () => {
+    if (!isDebateStarted) {
+      // 토론이 시작되지 않았다면 타이머 시작
+      const id = setInterval(() => {
+        setCount((prevCount) => prevCount - 1);
+      }, 1000);
+
+      setIsDebateStarted(true);
+
+      // 토론 시간이 종료되면 clearInterval을 호출하여 타이머를 멈춥니다.
+      setTimeout(() => {
+        clearInterval(id);
+        // 여기에서 추가적인 로직을 수행할 수 있습니다.
+      }, count * 1000);
+    }
+
+    if (userInput.trim() !== "") {
+      // 입력 필드를 즉시 비웁니다.
+      setUserInput("");
+
+      try {
+        const userMessage = { text: userInput, sender: "user" };
+        setMessages([...messages, userMessage]);
+
+        const answer = await debate(userInput);
+        const aiMessage = { text: answer, sender: "Ai" };
+
+        setAiAnswer(answer);
+        setAiMessages([...aiMessages.slice(0, -1), aiMessage]);
+        setConversation([...conversation, userMessage, aiMessage]);
+        setDebateContentsState(conversation);
+      } catch (error) {
+        console.error("데이터를 가져오는 중 에러 발생:", error);
+      }
+    }
+  };
+  
   return (
     <>
       <Div>
         <GlobalStyle />
         <SmallBackground src={smallBg} />
+        <Big>
+          <Part0>
+            <ChapTitle>Chapter 2.</ChapTitle>
+            <Title>토론 참여</Title>
+          </Part0>
 
-        <Part0>
-          <ChapTitle>Chapter 2.</ChapTitle>
-          <Title>토론 참여</Title>
-        </Part0>
+          <SameLine>
+            <Clock src={TimerVector} />
+            <TimeLeft>남은 시간 </TimeLeft>
+            <Timer> {formattedTime}</Timer>{" "}
+          </SameLine>
+        </Big>
         <Hr />
-
         <Container>
           <AiCharImg src={AiChar} />
           <HumanCharImg src={HumanChar} />
         </Container>
-
-        <CenteredContainer>
-          <OppositeMessageBubble className="push">
-            가상 국회의원들과 토론을 진행하세요. 먼저 의견을 내면 토론이
-            시작됩니다.
-          </OppositeMessageBubble>
-          {conversation.map((message, index) => {
-            const MessageComponent =
-              message.sender === "user" ? MessageBubble : OppositeMessageBubble;
-            return (
-              <MessageComponent
-                className={index % 2 === 0 ? "pull" : "push"}
-                key={index}
-              >
-                {message.text}
-              </MessageComponent>
-            );
-          })}
+        <CenteredContainer ref={containerRef}>
+          <MessageContainer>
+            <OppositeMessageBubble className="push">
+              가상 국회의원들과 토론을 진행하세요. 먼저 의견을 내면 토론이
+              시작됩니다.
+            </OppositeMessageBubble>
+            {conversation.map((message, index) => {
+              const MessageComponent =
+                message.sender === "user"
+                  ? MessageBubble
+                  : OppositeMessageBubble;
+              return (
+                <MessageComponent
+                  className={index % 2 === 0 ? "pull" : "push"}
+                  key={index}
+                >
+                  {message.text}
+                </MessageComponent>
+              );
+            })}
+          </MessageContainer>
         </CenteredContainer>
-
         <SendArea>
           <ContentInput
             type="text"
@@ -97,12 +172,48 @@ const WebChapter22 = () => {
           />
           <SendButton onClick={handleSendMessage}>전송</SendButton>
         </SendArea>
-        <Button>토론 끝내기</Button>
+        <Button onClick={moveToNext}>토론 끝내기</Button>
       </Div>
     </>
   );
 };
 
+const Big = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 210px;
+`;
+const Clock = styled.img``;
+const SameLine = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-left: 70%;
+  margin-bottom: -13%;
+`;
+const TimeLeft = styled.div`
+  color: #fff;
+  text-align: center;
+  font-family: "Pretendard Variable";
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-left: 8px;
+  margin-right: 18px;
+  min-width: 50%;
+`;
+const Timer = styled.div`
+  color: #fff;
+  text-align: center;
+  font-family: "Pretendard Variable";
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
 const Container = styled.div`
   display: flex;
   flex-direction: row;
@@ -122,66 +233,64 @@ const CenteredContainer = styled.div`
   margin-top: -32.5%;
   width: 65%;
   height: 450px;
-  margin-bottom: 90px;
+  margin-bottom: 40px;
   overflow: scroll;
   overflow-y: auto;
   overflow-x: hidden;
-  background-color: #000;
+`;
+
+const MessageContainer = styled.div`
+  margin-left: 5%;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
 
 const MessageBubble = styled.div`
-flex-direction: row;
+  align-self: flex-end;
   width: auto;
-  height: auto;
-  max-width: 300.976px;
+  max-width: 500.976px;
   flex-shrink: 0;
-  background-color: rgba(255, 198, 52, 0.7);
+  background-color: #d9bd6e;
   filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.1));
-  margin-bottom: 29.56px;
-  padding-top: 7.34px;
-  padding-bottom: 7.34px;
-  padding-left: 13.92px;
-  padding-right: 13.92px;
-  // margin-right: 0%;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 15px;
+
   &.pull:after {
-    align-self: flex-end;
     content: "";
     position: absolute;
-    top: 0px;
-    right: -40px;
+    top: 10%;
+    right: -13px;
+    border-top: 5px solid transparent;
+    border-left: 20px solid #d9bd6e;
     border-bottom: 10px solid transparent;
-    border-right: 20px solid transparent;
-    border-left: 20px solid #ffc634;
-    border-top: 0px solid #ffc634;
-    opacity: 0.7;
   }
 `;
+
 const OppositeMessageBubble = styled.div`
-flex-direction: row-reverse;
+  align-self: flex-start;
   width: auto;
-  height: auto;
-  max-width: 300.976px;
+  max-width: 500.976px;
   flex-shrink: 0;
-  background-color: rgba(246, 246, 246, 0.3);
+  background-color: #a7c1f6;
   filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.1));
-  margin-bottom: 29.56px;
-  padding-top: 7.34px;
-  padding-bottom: 7.34px;
-  padding-left: 13.92px;
-  padding-right: 13.92px;
-  // margin-left: -50%;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 15px;
+
   &.push:after {
-    align-self: flex-start;
     content: "";
     position: absolute;
-    top: 0px;
-    left: -20px;
-    border-top: 0px solid #f6f6f64d;
-    border-right: 20px solid #f6f6f64d;
-    border-left: 20px solid #transparent;
+    top: 10%;
+    left: -15px;
+    border-top: 5px solid transparent;
+    border-right: 20px solid #a7c1f6;
     border-bottom: 10px solid transparent;
   }
 `;
+
 const SendArea = styled.div`
   display: flex;
   flex-direction: row;
@@ -194,8 +303,8 @@ const ContentInput = styled.input`
   padding-left: 20px;
   font-size: 18px;
   border: none;
-  background: rgba(246, 246, 246, 0.3);
-  box-shadow: 0px 10px 100px rgba(0, 0, 0, 0.1); /* 그림자 추가 */
+  background: #acc5f6;
+  filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.1));
   &::placeholder {
     color: #fff;
   }
@@ -204,7 +313,6 @@ const ContentInput = styled.input`
 const SendButton = styled.button`
   width: 149px;
   height: 40px;
-  flex-shrink: 0;
   background-color: rgba(255, 198, 52, 0.8);
   color: white;
   border: none;

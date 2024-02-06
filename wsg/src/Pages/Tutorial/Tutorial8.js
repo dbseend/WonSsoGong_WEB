ae453672fc9fa6dfc3474d18fe0ef37703cb5c62
seol-wonjun <qiops@naver.com>
2024-02-06 21:56:49 +0900

    update Tutorial

---
+++ wsg/src/Pages/Tutorial/Tutorial8.js
@@ -0,0 +1,16 @@
+import React from "react";
+import WebFooter from "../../Components/Etc/WebFooter";
+import WebNavbar from "../../Components/Etc/WebNavbar";
+import WebTutorial8 from "../../Components/Tutorial/WebTutorial8";
+
+const Tutorial8 = () => {
+  return (
+    <div>
+      <WebNavbar />
+      <WebTutorial8 />
+      <WebFooter />
+    </div>
+  );
+};
+
+export default Tutorial8;
